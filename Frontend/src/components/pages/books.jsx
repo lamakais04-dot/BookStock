34096a3c6ddbf01ad1879bcb83ade0527da8a082
lamakais04-dot <lama.kais04@gmail.com>
2026-01-26 08:32:26 +0200

    added books page

--- Frontend/src/components/pages/books.jsx
+++ Frontend/src/components/pages/books.jsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import Books from '../services/books.js'
-
+import Bookitem from './bookitem.jsx'
+import '../csspages/books.css'
 export default function AllBooks() {
-  const [allbooks, setAllBooks] = useState([])
+    const [allbooks, setAllBooks] = useState([])
 
-  useEffect(() => {
-    async function getAllBooks() {
-      try {
-        const data = await Books.getBooks()   
-        setAllBooks(data)                     
-      } catch (err) {
-        console.error(err)
-      }
-    }
+    useEffect(() => {
+        async function getAllBooks() {
+            try {
+                const data = await Books.getBooks()
+                setAllBooks(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
 
-    getAllBooks()
-  }, [])
+        getAllBooks()
+    }, [])
 
-  return (
-    <div>
-      {allbooks.map(b => (
-        <h1 key={b.id}>{b.title}</h1>
-      ))}
-    </div>
-  )
+    return (
+        <div className="books-grid">
+            {allbooks.map(b => (
+                <Bookitem key={b.id} book={b} />
+            ))}
+        </div>
+    )
 }
