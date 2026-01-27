24d947969a826e772b217cb4083a293b31a56652
dianakayouf <dianakayouf@gmail.com>
2026-01-27 10:32:59 +0200

    enter

--- Frontend/src/components/layouts/layout/layout.jsx
+++ Frontend/src/components/layouts/layout/layout.jsx
@@ -5,7 +5,8 @@ import Navbar from '../header/navbar'
 import '../../csspages/layout.css'
 import Login from '../../pages/login'
 import HomePage from '../../pages/homePage'
-import signup from '../../pages/signup'import Signup from '../../pages/signup'
+import signup from '../../pages/signup'
+import Signup from '../../pages/signup'
 
 
 export default function Layout() {
