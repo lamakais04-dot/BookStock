4f1cdc695805d6caf85a030c60badeb77fd6d548
dianakayouf <dianakayouf@gmail.com>
2026-01-27 10:26:23 +0200

    added home page

--- Frontend/src/components/layouts/layout/layout.jsx
+++ Frontend/src/components/layouts/layout/layout.jsx
@@ -4,17 +4,20 @@ import { Routes, Route, Outlet } from 'react-router-dom'
 import Navbar from '../header/navbar'
 import '../../csspages/layout.css'
 import Login from '../../pages/login'
-
+import HomePage from '../../pages/homePage'
 
 export default function Layout() {
     return (
         <>
             <div className='layout'>
                 <Navbar></Navbar>
+                <div id="component">
                 <Routes>
                     <Route path='/book' Component={AllBooks} />
                     <Route path='/login' Component={Login}/>
+                    <Route path='/' Component={HomePage}/>
                 </Routes>
+                </div>
                 <Outlet />
             </div>
         </>
